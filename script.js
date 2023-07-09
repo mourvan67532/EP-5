c420d5e3eae58f054e58e081d2121f470a55c226
mourvan <124926548+mourvan67532@users.noreply.github.com>
2023-07-08 22:39:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function obrigado(){
+    alert ("Compra finalizada com sucesso")
+}
+function lixo(){
+    let el = document.getElementById('lixo_java');
+    el.style.cssText =
+    'display:none;';
+}
+function lixo2(){
+    let el = document.getElementById('lixo_java2');
+    el.style.cssText =
+    'display:none;';
+}
+function lixo3(){
+    let el = document.getElementById('lixo_java3');
+    el.style.cssText =
+    'display:none;';
+}
